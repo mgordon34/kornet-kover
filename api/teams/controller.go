549b14601b6f0483470ad59084bcdb3a561237c3
mgordon34 <coltfanman@gmail.com>
2024-11-12 17:53:57 -0700

    migrated teams controller to pgx

--- api/teams/controller.go
+++ api/teams/controller.go
@@ -1,48 +1,52 @@
 package teams
 
 import (
-	"github.com/lib/pq"
-    "github.com/mgordon34/kornet-kover/internal/storage"
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/mgordon34/kornet-kover/internal/storage"
 )
 
 func AddTeams(teams []Team) {
     db := storage.GetDB()
-	txn, _ := db.Begin()
-	_, err := txn.Exec(`
-	CREATE TEMP TABLE teams_temp
-	ON COMMIT DROP
-	AS SELECT * FROM teams
-	WITH NO DATA`)
+	txn, _ := db.Begin(context.Background())
+	_, err := txn.Exec(
+        context.Background(),
+        `CREATE TEMP TABLE teams_temp
+        ON COMMIT DROP
+        AS SELECT * FROM teams
+        WITH NO DATA`,
+    )
 	if err != nil {
 		panic(err)
 	}
 
-	stmt, err := txn.Prepare(pq.CopyIn("teams_temp", "index", "name"))
+    var teamsInterface [][]interface{}
+    for _, team := range teams {
+        teamsInterface = append(teamsInterface, []interface{}{team.Index, team.Name})
+    }
+
+	_, err = txn.CopyFrom(
+        context.Background(),
+        pgx.Identifier{"teams_temp"},
+        []string{"index", "name"},
+        pgx.CopyFromRows(teamsInterface),
+    )
 	if err != nil {
 		panic(err)
 	}
 
-	for _, t := range teams {
-		if _, err := stmt.Exec(t.Index, t.Name); err != nil {
-			panic(err)
-		}
-	}
-	if _, err := stmt.Exec(); err != nil {
-		panic(err)
-	}
-	if err := stmt.Close(); err != nil {
-		panic(err)
-	}
-
-	_, err = txn.Exec(`
-	INSERT INTO teams (index, name)
-	SELECT * FROM teams_temp
-	ON CONFLICT DO NOTHING`)
+	_, err = txn.Exec(
+        context.Background(),
+        `INSERT INTO teams (index, name)
+        SELECT * FROM teams_temp
+        ON CONFLICT DO NOTHING`,
+    )
 	if err != nil {
 		panic(err)
 	}
 
-	if err := txn.Commit(); err != nil {
+	if err := txn.Commit(context.Background()); err != nil {
 		panic(err)
 	}
 }
