e8fb917674a6f400faed7e5986b1447af0f1edea
mgordon34 <coltfanman@gmail.com>
2024-12-02 12:29:40 -0700

    Setting up prop picking function

--- internal/analysis/prop_selector.go
+++ internal/analysis/prop_selector.go
@@ -1,5 +1,24 @@
 package analysis
 
-type PropSelector struct {
+import (
+    "log"
 
+	"github.com/mgordon34/kornet-kover/api/odds"
+)
+
+type PropSelector struct {
+}
+
+type PropPick struct {
+    PropOdd         odds.PlayerOdds
+    Analysis
+}
+
+func (p PropSelector) PickProps(props map[string]odds.PlayerOdds, analyses []Analysis) ([]PropPick, error) {
+    var picks []PropPick
+    for _, analysis := range analyses {
+        log.Printf("Running analysis on %v", analysis.PlayerIndex)
+    }
+
+    return picks, nil
 }
