3418a6e829b40193c066de967fb6afb66f896fd7
mgordon34 <coltfanman@gmail.com>
2025-01-05 17:10:28 -0600

    Updated markoldpicksinvalid function to work with stratid of prop_selector

--- backend/internal/analysis/prop_selector.go
+++ backend/internal/analysis/prop_selector.go
@@ -120,7 +120,7 @@ func (p PropSelector) PickProps(props map[string]map[string]odds.PlayerOdds, ana
 
     if savePicks {
         models := p.convertToPicksModel(selectedPicks, date)
-        picks.MarkOldPicksInvalid(1, date)
+        picks.MarkOldPicksInvalid(p.StratId, date)
         err := picks.AddPropPicks(models)
         if err != nil {
             return selectedPicks, errors.New(fmt.Sprintf("Error getting saving picks: %v", err))
