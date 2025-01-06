184c69c64ac11a54b1a82171e6317e5cdd3e16dd
mgordon34 <coltfanman@gmail.com>
2025-01-06 11:17:17 -0600

    Updated date

--- frontend/quantcovers/app/portfolio/page.tsx
+++ frontend/quantcovers/app/portfolio/page.tsx
@@ -5,7 +5,7 @@ import PickList from '../components/PickList';
 // Fetch picks directly on the server side using `fetch`
 async function getPicks(): Promise<PropPick[]> {
   const userId = 1;
-  const res = await fetch(`${process.env.API_URL}/prop-picks?user_id=${userId}&date=2025-01-05`, {
+  const res = await fetch(`${process.env.API_URL}/prop-picks?user_id=${userId}&date=2025-01-06`, {
     method: 'GET',
     headers: {
       'Authorization': `Bearer ${process.env.JWT_TOKEN}`, // Or use session/cookies for auth
