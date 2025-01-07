f74dfd31ab98777acceeab9decbe93697bee64c5
mgordon34 <coltfanman@gmail.com>
2025-01-07 08:41:28 -0600

    Added functions to get predictions and diffs of right stat

--- frontend/quantcovers/app/components/PropPickItem.tsx
+++ frontend/quantcovers/app/components/PropPickItem.tsx
@@ -5,10 +5,30 @@ interface PropPickItemProps {
   pick: PropPick;
 }
 
+function getPrediction(pick: PropPick): number {
+    switch (pick.stat) {
+        case "points":
+            return pick.points;
+        case "rebounds":
+            return pick.rebounds;
+        case "assists":
+            return pick.assists;
+
+        default:
+            return pick.points;
+    }
+}
+
+function calculateDiff(pick: PropPick): number {
+    const prediction = getPrediction(pick)
+
+    return Math.abs(pick.line - prediction)
+}
+
 const PropPickItem: React.FC<PropPickItemProps> = ({ pick }) => {
   return (
     <li>
-      <p className="border-b border-border/40 text-1xl p-2">[{pick.num_games}] {pick.player_name} {pick.side} {pick.line} {pick.stat} - Prediction: {pick.points} Diff: {pick.points - pick.line}</p>
+      <p className="border-b border-border/40 text-1xl p-2">[{pick.num_games}] {pick.player_name} {pick.side} {pick.line} {pick.stat} - Prediction: {getPrediction(pick).toFixed(2)} Diff: {calculateDiff(pick).toFixed(2)}</p>
     </li>
   );
 };
