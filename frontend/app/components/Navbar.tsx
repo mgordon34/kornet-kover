93c1e0d2ad0fc1b4558ccc02bdfffa8aa65d9bac
mgordon34 <coltfanman@gmail.com>
2025-01-14 15:39:08 -0600

    commented out home for now

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -6,9 +6,11 @@ const Navbar = () => {
     <nav className="border-b border-border/40 text-white px-8 py-2 flex justify-between items-center">
       {/* Left Section */}
       <div className="flex space-x-8">
+        {/*
         <Link href="/" className="text-lg font-semibold hover:text-gray-400">
           Home
         </Link>
+        */}
         <Link href="/strategies" className="text-lg font-semibold hover:text-gray-400">
           Strategies
         </Link>
