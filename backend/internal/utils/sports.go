6153727ea9b8db7e003ca9309706d6863accb8ab
mgordon34 <coltfanman@gmail.com>
2025-05-08 17:55:23 -0500

    Added odds_api key

--- backend/internal/utils/sports.go
+++ backend/internal/utils/sports.go
@@ -10,15 +10,18 @@ const (
 type SportConfig struct {
 	Domain      string
 	BoxScoreURL string
+	OddsName    string
 }
 
 var SportConfigs = map[Sport]SportConfig{
 	NBA: {
 		Domain:      "https://www.basketball-reference.com",
 		BoxScoreURL: "/boxscores",
+		OddsName:    "basketball_nba",
 	},
 	MLB: {
 		Domain:      "https://www.baseball-reference.com",
 		BoxScoreURL: "/boxes",
+		OddsName:    "baseball_mlb",
 	},
 }
