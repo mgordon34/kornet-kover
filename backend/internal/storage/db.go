9176efe0d95aa06e3999f9a120ade066191b1ce9
mgordon34 <coltfanman@gmail.com>
2026-02-07 14:15:52 +0100

    Adopted direnv

--- backend/internal/storage/db.go
+++ backend/internal/storage/db.go
@@ -7,7 +7,6 @@ import (
 	"sync"
 
 	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/joho/godotenv"
 )
 
 var (
@@ -25,11 +24,6 @@ func Close() {
 
 func GetDB() *pgxpool.Pool {
 	pgOnce.Do(func() {
-		err := godotenv.Load()
-		if err != nil {
-			log.Fatal("Error loading .env file")
-		}
-
 		dbpool, err := pgxpool.New(context.Background(), os.Getenv("DB_URL"))
 		if err != nil {
 			log.Fatalf("Unable to connect to database: %v\n", err)
